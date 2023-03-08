a8dc4456198b2aacc633f6188d09e4cf9f2c1883
Danny Tran <danieltran2016@gmail.com>
2023-03-08 17:49:03 -0600

    commiting, tried one way to dospaly data, not wokring, will try after class

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,19 @@
-var API_Key = "923a17bd8e04070a7361ee88ec3d1f23xx"
+var API_Key = "923a17bd8e04070a7361ee88ec3d1f23"
+var temp_list = document.querySelector('tem');
 
 function getWeather(){
   var search = document.getElementById("cityInput");
-  var cityname  = document.getElementById("time-zone");
-  cityname.innerHTML = search.value;
   console.log(search.value);
 
 fetch("https://api.openweathermap.org/data/2.5/forecast?q="+search.value+"&appid="+API_Key)
 .then(response => response.json())
-.then(data => {
-    console.log(data) 
-  })
+.then(data => {console.log(data)})
 }
 
+function displayWeather(data){
+var {name} = data.city;
+var {temp, humidity,} = data.main;
+var{speed} = data.wind 
+console.log(name , temp, humidity, speed)
+}
+displayWeather();
